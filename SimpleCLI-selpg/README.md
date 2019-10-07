462fb912d91e3f48132fefb5b78a1c4aeffa23b2
fsq <1218424633@qq.com>
2019-10-07 21:58:20 +0800

    final commit

---
+++ SimpleCLI-selpg/README.md
@@ -0,0 +1,105 @@
+# CLI 命令行实用程序开发基础
+
+### 1、概述
+
+  CLI（Command Line Interface）实用程序是Linux下应用开发的基础。正确的编写命令行程序让应用与操作系统融为一体，通过shell或script使得应用获得最大的灵活性与开发效率。Linux提供了cat、ls、copy等命令与操作系统交互；go语言提供一组实用程序完成从编码、编译、库管理、产品发布全过程支持；容器服务如docker、k8s提供了大量实用程序支撑云服务的开发、部署、监控、访问等管理任务；git、npm等都是大家比较熟悉的工具。尽管操作系统与应用系统服务可视化、图形化，但在开发领域，CLI在编程、调试、运维、管理中提供了图形化程序不可替代的灵活性与效率。
+
+### 2、实验内容
+
+使用 golang 开发 [开发 Linux 命令行实用程序](https://www.ibm.com/developerworks/cn/linux/shell/clutil/index.html) 中的 **selpg**
+
+提示：
+
+1. 请按文档 **使用 selpg** 章节要求测试你的程序
+2. 请使用 pflag 替代 goflag 以满足 Unix 命令行规范， 参考：[Golang之使用Flag和Pflag](https://o-my-chenjian.com/2017/09/20/Using-Flag-And-Pflag-With-Golang/)
+3. golang 文件读写、读环境变量，请自己查 os 包
+4. “-dXXX” 实现，请自己查 `os/exec` 库，例如案例 [Command](https://godoc.org/os/exec#example-Command)，管理子进程的标准输入和输出通常使用 `io.Pipe`，具体案例见 [Pipe](
+
+### 3、设计说明
+
+1. 首先，引入pflag（程序中将其重命名为了flag）并定义一个结构体存储seplg所有可能用到的命令。
+
+   ![1](assets/1.png)
+
+2. 程序运行后利用pflag对命令行输入进行解析获取需要的参数。
+
+   ![2](assets/2.png)
+
+3. 获取参数后检查其是否合理，不合理则报错退出。
+
+   ![3](assets/3.png)
+
+4. 参数合理后开始执行命令。
+
+   先确定读取数据的位置，如果不是标准输出则需要打开该路径。
+
+   ![4](assets/4.png)
+
+   之后根据输出路径进行数据的读取和输出
+
+   ![5](assets/5.png)
+
+5. 读取并输出数据。
+
+   首先需要根据是否按分页符分页确定每次读取一页数据还是读取一行数据，每次读取后判断是否为目标位置数据，是则输出并继续读取直至文件末尾。
+
+   ```go
+   func readAndWrite(fwrite interface{}, fread *os.File, start int, end int, lines int, feed bool) {
+   	pageCount := 0                   //所读的页所在的页数
+   	lineCount := 0                   //当前页所读的行数
+   	buffer := bufio.NewReader(fread) //存储已读取数据的缓冲区
+   
+   	var aline, apage string //读取的一行/页数据
+   	var err error           //检测是否出错
+   	fmt.Println("We have read:")
+   	if feed { //如果是按照“\f”分页读取
+   		for true {
+   			apage, err = buffer.ReadString('\f')            //每次读一页
+   			pageCount++                                     //更新页数
+   			if (pageCount >= start) && (pageCount <= end) { //判断是否可写
+   				write(fwrite, apage)
+   			}
+   			if err == io.EOF || pageCount > end { //先写入文件再判断是否为文件末尾，避免最后一页或者一行因EOF而无法写入。
+   				break
+   			}
+   		}
+   	} else {
+   		pageCount = 1 //第一次读确定为第一页
+   		for true {
+   			aline, err = buffer.ReadString('\n')
+   			lineCount++            //每次读行数加一
+   			if lineCount > lines { //行数超过一页的行数，则行数归一，页数加一
+   				pageCount++
+   				lineCount = 1
+   			}
+   			if (pageCount >= start) && (pageCount <= end) { //判断是否可写
+   				write(fwrite, aline)
+   			}
+   			if err == io.EOF || pageCount > end {
+   				break
+   			}
+   		}
+   	}
+   
+   	if pageCount < start { //假如文件页数不够开始写
+   		checkError(err, "StartPage greater than the number of total pages!Nothing to write! ")
+   	} else if pageCount < end { //假如文件页数达不到所要写的页数
+   		checkError(err, "EndtPage greater than the number of total pages!Incomplete read and write!")
+   	}
+   
+   	//fmt.Printf("\nWriting complete!\nSource: %s\t\t\tTarget:%s",source,target)
+   }
+   
+   ```
+
+### 4、测试结果
+
+
+
+### 5、参考资料
+
+1. <https://pmlpml.github.io/ServiceComputingOnCloud/ex-cli-basic>
+2. <https://blog.csdn.net/chenbaoke/article/details/42556949>
+3. <https://blog.csdn.net/PeanutDo1t/article/details/83020872>
+4. <https://www.ibm.com/developerworks/cn/linux/shell/clutil/index.html>
+
--- SimpleCLI-selpg/selpg/selpg.go
+++ SimpleCLI-selpg/selpg/selpg.go
@@ -7,154 +7,175 @@ import (
 	"os"
 	"os/exec"
 
-	"github.com/spf13/pflag"
+	flag "github.com/spf13/pflag" //引入pflag并重命名为flag
 )
 
-var startPage, endPage, pageLen int
-var formFeed bool
-var outputPath string
-var programName string
-var inputFileName string
+//Arguments 存储参数
+type Arguments struct {
+	startPage   int    //读取开始的页数
+	endPage     int    //读取结束的页数
+	pathToRead  string //读取数据的路径
+	pathToWrite string //写入数据的路径
+	pageLines   int    //读取的数据以多少行作为一页
+	sepWithFeed bool   //是否已换页符作为分页依据，与pageLines互斥
 
-var cmd *exec.Cmd
-var out io.WriteCloser //use to write to or close I/O devices
-var in io.ReadCloser   //use to read or close I/O device
-var buff *bufio.Reader
-
-/*SomeInit function init the binding params*/
-func SomeInit() {
-	//bind params and read values
-	pflag.IntVarP(&startPage, "start page", "s", 0, "The start page of  the file.")
-	pflag.IntVarP(&endPage, "end page", "e", 0, "The end page of the file.")
-	pflag.IntVarP(&pageLen, "page length", "l", 72, "Number of lines in one page.")
-	pflag.BoolVarP(&formFeed, "form feed", "f", false, "The page will end until meet a form feed.")
-	pflag.StringVarP(&outputPath, "output file", "d", "", "Path of the destinate device")
-	pflag.Usage = func() { //output how to use this tools
-		fmt.Printf("Usage of %s:\n", programName)
-		fmt.Printf("-s\t--Start page of the file,must be chosed,default 0.\n-e\t--End page of the file,must be chosed,default 0.\n-l\t--Lines of one page,optional,default 72.\n-f\t--Page will end with form feed,default false.\n-d\t--Output to which device,default null.\n")
-		//pflag.PrintDefaults()
-	}
 }
 
-/*IsArgsLegal function detect if the values of the params are legal*/
-func IsArgsLegal() { //judge if there is more than 3 args for the format of "program -sNumber -eNumber"
-	if len(os.Args) < 3 || os.Args[1] != "-s" || os.Args[3] != "-e" {
-		fmt.Printf("\nError: must be the format as ./CLI -s Number -e Number\n ")
-		pflag.Usage()
-		os.Exit(1)
-	}
-	IntMax := 1<<32 - 1
-	if startPage < 1 || startPage > IntMax || endPage < 1 || endPage > IntMax || endPage < startPage {
-		fmt.Printf("Error: the page number is not legal.\n ")
-		pflag.Usage()
-		os.Exit(2)
-	}
-	if pageLen < 1 || pageLen > IntMax {
-		fmt.Printf("Error: the lines in one page is not legal.\n")
-		pflag.Usage()
-		os.Exit(3)
-	}
-	if pflag.NArg() == 1 { //get non-flag param, only input filename is permitted
-		_, err := os.Stat(pflag.Args()[0])
-		if err != nil && os.IsNotExist(err) {
-			fmt.Printf("Error: the file %s is not exist.\n", pflag.Args()[0])
-			pflag.Usage()
-			os.Exit(4)
-		}
-		inputFileName = pflag.Args()[0]
+//var fread,fwrite *os.File //文件读写流
+//var args Arguments
+var source, target string
+
+func checkError(err error, wrong string) { //处理错误
+	if err != nil { //若有错，则输出错误信息
+		fmt.Fprintf(os.Stderr, "\nError:%s:", wrong)
+		os.Exit(0)
 	}
 }
-
-/*DeviceToInAndOut function decides which device to input or output*/
-func DeviceToInAndOut() {
-
-	if inputFileName == "" {
-		in = os.Stdin
-	} else {
-		var err error
-		in, err = os.Open(inputFileName)
-		if err != nil {
-			fmt.Println(err)
-			os.Exit(6)
-		}
-	}
-	buff = bufio.NewReader(in)
-
-	if outputPath == "" {
-		out = os.Stdout
-	} else {
-		var err error
-		out, err = os.OpenFile(outputPath, os.O_CREATE|os.O_RDWR, 777) //let cli output to this file
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "ERROR: %s\n", err)
-			os.Exit(7)
-		}
-	}
+func writePath(pathToWrite string) io.WriteCloser { //找到写入的路径
+	cmd := exec.Command("lp", "-d"+pathToWrite) //利用lp打开管道
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	fwrite, err := cmd.StdinPipe()
+	checkError(err, "StdinPipe() open failed!")
+	return fwrite
 }
 
-/*ReadAndWrite function complete the work of input and output*/
-func ReadAndWrite() {
-	var pageNum int
-	if formFeed {
-		pageNum = 1
-		for {
-			line, err := buff.ReadString('\f')
-			if err != nil && err != io.EOF {
-				fmt.Println(err)
-				os.Exit(9)
+//读取数据并将数据写到指定路径
+
+func write(fwrite interface{}, toWrite string) {
+	var writeError error
+	if isStd, ok := fwrite.(*os.File); ok { //判断是否为标准输出
+		_, writeError = fmt.Fprintf(isStd, "%s", toWrite) //是则写入，返回写入信息
+	} else if isPipe, ok := fwrite.(io.WriteCloser); ok { //判断是否为管道
+		_, writeError = isPipe.Write([]byte(toWrite))
+	} else { //两者都不是则表明发生错误
+		fmt.Fprintf(os.Stderr, "\nError:Fstream to write is invalid.Please check your arguments[-d]! ")
+		os.Exit(8)
+	}
+	checkError(writeError, "Error happend when writing!.") //判断写入过程中是否发生错误
+}
+func readAndWrite(fwrite interface{}, fread *os.File, start int, end int, lines int, feed bool) {
+	pageCount := 0                   //所读的页所在的页数
+	lineCount := 0                   //当前页所读的行数
+	buffer := bufio.NewReader(fread) //存储已读取数据的缓冲区
+
+	var aline, apage string //读取的一行/页数据
+	var err error           //检测是否出错
+	fmt.Println("We have read:")
+	if feed { //如果是按照“\f”分页读取
+		for true {
+			apage, err = buffer.ReadString('\f')            //每次读一页
+			pageCount++                                     //更新页数
+			if (pageCount >= start) && (pageCount <= end) { //判断是否可写
+				write(fwrite, apage)
 			}
-			line += "\f"
-			fmt.Println(line)
-			if err == io.EOF {
+			if err == io.EOF || pageCount > end { //先写入文件再判断是否为文件末尾，避免最后一页或者一行因EOF而无法写入。
 				break
 			}
-			pageNum++
-			if pageNum >= startPage && pageNum <= endPage {
-				_, err := out.Write([]byte(line))
-				if err != nil {
-					fmt.Println(err)
-					os.Exit(10)
-				}
-			}
 		}
 	} else {
-		lineNum := 1
-		pageNum = 1
-		for {
-			line, _, err := buff.ReadLine()
-			if err != nil && err != io.EOF {
-				fmt.Println(err)
-				os.Exit(9)
+		pageCount = 1 //第一次读确定为第一页
+		for true {
+			aline, err = buffer.ReadString('\n')
+			lineCount++            //每次读行数加一
+			if lineCount > lines { //行数超过一页的行数，则行数归一，页数加一
+				pageCount++
+				lineCount = 1
 			}
-			if err == io.EOF {
+			if (pageCount >= start) && (pageCount <= end) { //判断是否可写
+				write(fwrite, aline)
+			}
+			if err == io.EOF || pageCount > end {
 				break
-			}
-			lineNum++
-			if lineNum > pageLen {
-				lineNum = 1
-				pageNum++
-			}
-			if pageNum >= startPage && pageNum <= endPage {
-				_, err := out.Write(append([]byte(line), []byte("\n")...))
-				if err != nil {
-					fmt.Println(err)
-					os.Exit(10)
-				}
 			}
 		}
 	}
-	in.Close()
-	out.Close()
-	if pageNum < startPage || pageNum < endPage {
-		fmt.Printf("Error: the page index is not in the range.\n")
+
+	if pageCount < start { //假如文件页数不够开始写
+		checkError(err, "StartPage greater than the number of total pages!Nothing to write! ")
+	} else if pageCount < end { //假如文件页数达不到所要写的页数
+		checkError(err, "EndtPage greater than the number of total pages!Incomplete read and write!")
+	}
+
+	//fmt.Printf("\nWriting complete!\nSource: %s\t\t\tTarget:%s",source,target)
+}
+
+func run(args *Arguments) { //开始运行指令
+	fread := os.Stdin
+	source = "os.stdin"
+	if len(args.pathToRead) > 0 { //args.pathToRead  == ""  不行
+		var err error
+		fread, err = os.Open(args.pathToRead) //打开该路径
+		if err != nil {                       //失败则报错退出
+			fmt.Fprintf(os.Stderr, "Error:File open failed! Please check the filename!")
+			os.Exit(0)
+		}
+		source = args.pathToRead
+	}
+
+	/*fwrite := os.Stdout
+	if args.pathToWrite != "" {//   args.pathToWrite  == nil
+		fwrite = writePath(args.pathToWrite)
+	}
+	readAndWrite(fwrite, fread, args.startPage, args.endPage, args.pageLines, args.sepWithFeed)*/
+
+	if len(args.pathToWrite) == 0 { //读取数据，如果没有指定输出路径，则输出到stdout
+		target = "os.Stdout"
+		readAndWrite(os.Stdout, fread, args.startPage, args.endPage, args.pageLines, args.sepWithFeed)
+	} else { //否则输出到指定路径
+		target = args.pathToWrite
+		readAndWrite(writePath(args.pathToWrite), fread, args.startPage, args.endPage, args.pageLines, args.sepWithFeed)
+	}
+}
+func checkArgs(args *Arguments) { //检查命令参数是否正确，若有错则报错并退出
+	fmt.Println("Start to check the command!")
+	if (args.startPage == 0) || (args.endPage == 0) { //参数不可少或者为0
+		fmt.Fprintf(os.Stderr, "\nError:StartPage and endPage are necessary and should be greater than 0!")
+		os.Exit(0)
+	} else if args.startPage > args.endPage { //开始页不可大于结束页
+		fmt.Fprintf(os.Stderr, "\nError:The startPage can't be bigger than the endPage!")
+		os.Exit(0)
+	} else if (args.sepWithFeed == true) && (args.pageLines != 72) { //以分页符分页和页行数不可同时出现（根据参考资料链接4）
+		fmt.Fprintf(os.Stderr, "\nError:The command -l and -f are exclusive!")
+		os.Exit(0)
+	} else if args.pageLines <= 0 { //页行数不能为0
+		fmt.Fprintf(os.Stderr, "\nError:The pageLines should be greater than 0! ")
+		os.Exit(0)
+	}
+	fmt.Println("Command valid! Start to run!")
+}
+
+func readArgs(args *Arguments) { //读取命令行参数
+	//绑定需要读取的，带前缀的参数
+	flag.IntVarP(&(args.startPage), "startPage", "s", 0, "Start page to read")
+	flag.IntVarP(&(args.endPage), "endPage", "e", 0, "End page to read")
+	flag.StringVarP(&(args.pathToWrite), "pathToWrite", "d", "", "Where to write the read data")
+	flag.IntVarP(&(args.pageLines), "pageLines", "l", 72, "Number of lines of one page")
+	flag.BoolVarP(&(args.sepWithFeed), "sepWithFeed", "f", false, "Seperates pages with \"\\f\"")
+	//函数用法
+	flag.Usage = func() {
+		fmt.Println("Usage of selpg:")
+		fmt.Println("[NECESSARY]")
+		fmt.Println("-s\t--Start page to read, default 0.")
+		fmt.Println("-e\t--End page to read, default 0.")
+		fmt.Println("[OPTIONAL]")
+		fmt.Println("-d\t--Where to write the read data, default stdout.")
+		fmt.Println("-l\t--Number of lines of one page, default 72. Exclusive with -f")
+		fmt.Println("-f\t--Seperates pages with \"\\f\", default false.")
+		fmt.Println("File to read.  -Just input the name of the file in your command.Default stdin")
+	}
+	//解析命令行参数
+	flag.Parse()
+	//假如还有剩下的参数,传给pathToRead
+	if len(flag.Args()) > 0 {
+		args.pathToRead = string(flag.Args()[0])
 	}
 }
 
 func main() {
-	programName = os.Args[0] //get the name of the program
-	SomeInit()               //init params
-	pflag.Parse()
-	IsArgsLegal()
-	DeviceToInAndOut()
-	ReadAndWrite()
+	var args Arguments
+	args.pathToRead = "" //路径预先初始化为空
+	readArgs(&args)      //读取参数
+	checkArgs(&args)     //检查参数是否合理
+	run(&args)           //执行命令
 }
